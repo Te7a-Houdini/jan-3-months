a406d5390f0bad11c4d05e5a73a697db8d905058
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2020-01-04 10:35:59 +0200

    Add PostResoure

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -5,30 +5,18 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Post;
-
+use App\Http\Resources\PostResource;
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
-        $tempArr = [];
-
-        foreach($posts as $post){
-            $tempArr [] = [
-                'id' => $post->id,
-                'title' => $post->title,
-            ];
-        }
-
-        return $tempArr;
+        return PostResource::collection(Post::all());
     }
 
     public function show($id)
     {
         $post = Post::find($id);
-        return [
-            'id' => $post->id,
-            'title' => $post->title,
-        ];
+
+        return new PostResource($post);
     }
 }
