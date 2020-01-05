fe8b3f71d159de9862d6aa65545c7d1006c3aa4e
Te7a-Houdini <ahmedabdelftah95165@gmail.com>
2020-01-05 15:08:21 +0200

    Add login with github

--- routes/web.php
+++ routes/web.php
@@ -81,3 +81,6 @@ Route::get('/posts/{post}', 'PostController@show')->name('posts.show');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('login/github', 'Auth\LoginController@redirectToProvider');
+Route::get('login/github/callback', 'Auth\LoginController@handleProviderCallback');
